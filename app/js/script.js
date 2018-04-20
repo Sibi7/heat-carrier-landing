d0bd0d271b374435e5fefcc11a940d4bc1d6df97
Sibi7 <Sibi_7@mail.ru>
2018-04-20 16:46:53 +0300

    section 10 finish desktop

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
+    //
+    // $('.clients__slider-container').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     fade: true,
+    //     asNavFor: '.clients__slider-nav'
+    // });
+    // $('.clients__slider-nav').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.clients__slider-container',
+    //     dots: false,
+    //     centerMode: true,
+    //     focusOnSelect: true
+    // });
 
 });
