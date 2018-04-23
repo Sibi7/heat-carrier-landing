f7f8e8274c6c5b5791dd67588ee75947caf0953a
Sibi7 <Sibi_7@mail.ru>
2018-04-23 20:25:00 +0300

    link scroll

--- app/js/script.js
+++ app/js/script.js
@@ -14,5 +14,13 @@ $(document).ready(function () {
                 items:1
             }
         }
-    })
+    });
+    $(document).on('click', '.header__nav a', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        var target = $(href);
+        var top = target.offset().top;
+        $('html,body').animate({scrollTop: top}, 1000);
+        return false;
+    });
 });
